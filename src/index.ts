a890a5d4c10b46942364199d4b2b28ff70050a39
Tugrul Topuz <tugrultopuz@gmail.com>
2024-05-02 05:44:50 +0300

    jsr docs update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
 
+/**
+ * Represents a callback function used for string replacement.
+ * @param match - The matched substring.
+ * @param groups - An array of captured groups in the regex match.
+ * @param index - The starting index of the match in the input string.
+ * @param input - The original input string.
+ * @returns A Promise resolving to the replacement string.
+ */
 export type ReplaceCallback = (match: string, groups: string[], index: number, input: string) => Promise<string>;
 
+/**
+ * Asynchronously replaces substrings in a string that match a specified regular expression
+ * with the results of a specified callback function.
+ * @param str - The input string to perform replacements on.
+ * @param regex - The regular expression pattern to match substrings against.
+ * @param callback - The callback function that produces the replacement substring.
+ * @param limit - Optional. The maximum number of replacements to make concurrently. Default is 0 (no limit).
+ * @returns A Promise resolving to the modified string after replacements.
+ */
 export async function replace(str: string, regex: RegExp, callback: ReplaceCallback, limit: number = 0): string {
 
     let last = 0;
